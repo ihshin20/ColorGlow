8c5ac1ab4ba537ca9b4bda17ecba76b09dd27a8b
hyunhwa-song <147256497+hyunhwa-song@users.noreply.github.com>
2024-04-12 19:45:34 +0900

    Merge pull request #14 from ihshin20/hungry
    mainFragment 디자인하고 naviactivity fragment replace 에서 navicomponent 이용하고 homeFragment에는 일단은 카메라 찍어서 진단 하는거 함

--- app/src/main/java/com/example/mycolor/Fragment/MyPageFragment.kt
+++ app/src/main/java/com/example/mycolor/Fragment/MyPageFragment.kt
@@ -1,61 +1,39 @@
 
 package com.example.mycolor.Fragment
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.example.mycolor.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MyPageFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class MyPageFragment : Fragment(R.id.myPageFragment) {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class MyPageFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_my_page, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MyPageActivity.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MyPageFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // ImageView 로컬 변수에 할당
+        val logoimageView = view.findViewById<ImageView>(R.id.logoimageView)
+        logoimageView.setImageResource(R.drawable.logoimage)
+        val baseimageView = view.findViewById<ImageView>(R.id.baseimageView)
+        baseimageView.setImageResource(R.drawable.killcover)
+        val lipimageView = view.findViewById<ImageView>(R.id.lipimageView)
+        // 립 제품 이미지 뷰에 클릭 리스너를 설정
+        lipimageView.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://clubclio.co.kr/shop/goodsView/0000002419"))
+            startActivity(intent)
+        }
+
     }
 }
