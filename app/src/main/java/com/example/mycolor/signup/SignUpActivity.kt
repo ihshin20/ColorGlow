cf40a5c932cbaffb080857ec8e218a2900aa0802
ihshin20 <ianfortm@gmail.com>
2024-04-30 20:00:12 +0900

    AWS -> GCP으로 Server 이동
    GCP Server에 RGB 이미지 데이터로 학습된 모델 적용
    
    FireStore연동
    resultFragment에서 과거 진단 결과 보는 로직 추가 필요
    
    - mypage + login + sih_add_fireStore + Dy_add_fireStore
    
    - optimization
    : remove Actionbar, picture using camera directly rotation, fixed Light mode, rename app, change mipmap, layout modified, remove deprecated methods, add dialog, modify navigation, delete HomeFragment activity, layout, nav graph
    
    - 리사이클러 뷰 수정 및 클릭리스너 추가, ResultFragment.kt 사진 추가
    
    - resultFragment.kt, DetailResultActivity.kt 로직 완성

--- app/src/main/java/com/example/mycolor/signup/SignUpActivity.kt
+++ app/src/main/java/com/example/mycolor/signup/SignUpActivity.kt
@@ -1,8 +1,11 @@
 package com.example.mycolor.signup
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -67,5 +70,18 @@ class SignUpActivity : AppCompatActivity() {
                 }
         }
 
+    }
+
+    // 화면 터치시 키패드 숨기기
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            val view = currentFocus
+            if (view != null) {
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(view.windowToken, 0)
+                view.clearFocus()
+            }
+        }
+        return super.dispatchTouchEvent(event)
     }
 }
