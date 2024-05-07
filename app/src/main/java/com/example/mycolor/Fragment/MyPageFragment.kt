d00c4fc390f14d51f7cb61d87d7b03e3e6ef6d2e
Jiye Lee <80204749+dlwldP@users.noreply.github.com>
2024-05-07 14:36:16 +0900

    mypage에 제품글 불러오기4(text only)

--- app/src/main/java/com/example/mycolor/Fragment/MyPageFragment.kt
+++ app/src/main/java/com/example/mycolor/Fragment/MyPageFragment.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,33 +11,32 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.example.mycolor.R
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
-import com.google.firebase.storage.FirebaseStorage
 import java.text.SimpleDateFormat
 import java.util.Locale
 
 class MyPageFragment : Fragment() {
+
     private lateinit var uid: String
+
     private lateinit var baseProductTextViews: List<TextView>
     private lateinit var lipProductTextViews: List<TextView>
     private lateinit var eyeProductTextViews: List<TextView>
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_my_page, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initializeViews(view)
-        setupFirebaseAuth()
-    }
-
-    private fun initializeViews(view: View) {
         baseProductTextViews = listOf(
             view.findViewById(R.id.baseproductTextView_11),
             view.findViewById(R.id.baseproductTextView_22),
@@ -54,88 +52,29 @@ class MyPageFragment : Fragment() {
             view.findViewById(R.id.eyeproductTextView_22),
             view.findViewById(R.id.eyeproductTextView_33)
         )
-        setupProductImageViews(view)
-    }
 
-    private fun setupFirebaseAuth() {
+        //uid 가져오기
         val auth = FirebaseAuth.getInstance()
+
         val user = auth.currentUser
         if (user != null) {
             uid = user.uid
-            fetchUserUid(uid) { result ->
-                if (result != null) {
-                    updateProductDetails(result)
-                } else {
-                    Log.d("MyPageFragment", "Failed to fetch result or no data found.")
-                }
-            }
+            Log.d("FirebaseAuth", "User UID: $uid")
         } else {
             Log.d("FirebaseAuth", "No user is currently signed in.")
         }
-    }
 
-    private fun fetchUserUid(uid: String, callback: (String?) -> Unit) {
-        val diagnosticRef = FirebaseFirestore.getInstance().collection("User").document(uid).collection("results")
-        diagnosticRef.orderBy("date", Query.Direction.DESCENDING).limit(1)
-            .get()
-            .addOnSuccessListener { documents ->
-                if (documents.isEmpty) {
-                    Log.d("Firestore", "No documents found")
-                    callback(null)
-                } else {
-                    val document = documents.documents.first()
-                    val result = document.getString("results")
-                    callback(result)
-                }
-            }
-            .addOnFailureListener { exception ->
-                Log.w("Firestore", "Error getting documents: ", exception)
-                callback(null)
-            }
-    }
-
-    private fun updateProductDetails(res: String) {
-        val toneRef = FirebaseFirestore.getInstance().collection("Tone").document(res)
-        toneRef.get().addOnSuccessListener { document ->
-            if (document.exists()) {
-                extractAndDisplayProductDetails(document)
-            } else {
-                Log.d("Firestore", "No Tone document found")
-            }
-        }.addOnFailureListener { exception ->
-            Log.w("Firestore", "Error getting Tone document: ", exception)
-        }
-    }
-
-    private fun extractAndDisplayProductDetails(document: DocumentSnapshot) {
-        val productDescriptions = document.get("제품설명") as? List<Map<String, Any>> ?: listOf()
-        displayProductDetails(productDescriptions, "베이스", baseProductTextViews)
-        displayProductDetails(productDescriptions, "립", lipProductTextViews)
-        displayProductDetails(productDescriptions, "아이", eyeProductTextViews)
-    }
-
-    private fun displayProductDetails(descriptions: List<Map<String, Any>>, category: String, views: List<TextView>) {
-        val categoryMap = descriptions.find { it.containsKey(category) }?.get(category) as? Map<String, Any> ?: mapOf()
-        categoryMap.let { products ->
-            views.forEachIndexed { index, textView ->
-                val productKey = "${category}제품${index + 1}"
-                val productInfo = products[productKey] as? Map<String, Any>
-                val productName = productInfo?.get("제품이름") as? String ?: "제품명 정보 없음"
-                val productBrand = productInfo?.get("브랜드") as? String ?: "브랜드 정보 없음"
-                val productPrice = productInfo?.get("가격") as? String ?: "가격 정보 없음"
-                textView.text = "$productName\n$productBrand\n$productPrice"
-            }
-        }
-    }
-
-    private fun setupProductImageViews(view: View) {
+        //기존코드
         val logoimageView = view.findViewById<ImageView>(R.id.logoimageView)
         logoimageView.setImageResource(R.drawable.logoimage)
         val baseimageView = view.findViewById<ImageView>(R.id.baseimageView)
         baseimageView.setImageResource(R.drawable.killcover)
         val lipimageView = view.findViewById<ImageView>(R.id.lipimageView)
         lipimageView.setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://clubclio.co.kr/shop/goodsView/0000002419"))
+            val intent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://clubclio.co.kr/shop/goodsView/0000002419")
+            )
             startActivity(intent)
         }
         val eyeimageView = view.findViewById<ImageView>(R.id.eyeimageView)
@@ -147,5 +86,116 @@ class MyPageFragment : Fragment() {
                 activity?.finish()
             }, 1000)
         }
+
+        fetchUserUid(uid) { result ->
+            if (result.startsWith("Error") || result == "No documents found") {
+                Toast.makeText(context, "Failed to load data: $result", Toast.LENGTH_LONG).show()
+            } else {
+                updateProductDetails(result)
+            }
+        }
+    }
+
+    //callback 함수 사용
+    fun fetchUserUid(uid: String, callback: (String) -> Unit) {
+        val diagnosticRef =
+            FirebaseFirestore.getInstance().collection("User").document(uid).collection("results")
+        diagnosticRef.orderBy("date", Query.Direction.DESCENDING).limit(1)
+            .get()
+            .addOnSuccessListener { documents ->
+                if (documents.isEmpty) {
+                    Log.d("Firestore", "No documents found")
+                    callback("No documents found")
+                } else {
+                    var result: String? = null
+                    for (document in documents) {
+                        result = document.getString("result") ?: "No result found"
+                    }
+                    result?.let { callback(it) }
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("Firestore", "Error getting documents: ", exception)
+                callback("Error getting documents: ${exception.message}")
+            }
+    }
+
+    private fun updateProductDetails(result: String) {
+        val toneRef = FirebaseFirestore.getInstance().collection("Tone").document(result)
+        Log.d("FirestoreDebug", "Attempting to fetch document with ID: $result") // 문서 ID 로그 추가
+
+        toneRef.get()
+            .addOnSuccessListener { document ->
+                if (!document.exists()) {
+                    Log.d("FirestoreDebug", "No document found with ID: $result") // 문서가 없을 때 로그
+                    Toast.makeText(
+                        context,
+                        "No data available for this product.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    Log.d(
+                        "FirestoreDebug",
+                        "Document successfully fetched with ID: $result"
+                    ) // 문서가 있을 때 로그
+                    val productDescriptions =
+                        document.get("제품설명") as? List<Map<String, Any>> ?: listOf()
+                    Log.d(
+                        "FirestoreDebug",
+                        "Product descriptions found: $productDescriptions"
+                    ) // 제품 설명 로그
+
+                    val baseMap = productDescriptions.find { it.containsKey("베이스") }
+                        ?.get("베이스") as? Map<String, Any> ?: mapOf()
+                    baseMap.let { products ->
+                        baseProductTextViews.forEachIndexed { index, textView ->
+                            val productKey = "베이스제품${index + 1}"
+                            val productInfo = products[productKey] as? Map<String, Any>
+                            val productName = productInfo?.get("제품이름") as? String ?: "제품명 정보 없음"
+                            val productBrand = productInfo?.get("브랜드") as? String ?: "브랜드 정보 없음"
+                            val productPrice = productInfo?.get("가격") as? String ?: "가격 정보 없음"
+                            textView.text = "$productName\n$productBrand\n$productPrice"
+                        }
+                    }
+
+                    val lipMap = productDescriptions.find { it.containsKey("립") }
+                        ?.get("립") as? Map<String, Any> ?: mapOf()
+                    lipMap.let { products ->
+                        lipProductTextViews.forEachIndexed { index, textView ->
+                            val productKey = "립제품${index + 1}"
+                            val productInfo = products[productKey] as? Map<String, Any>
+                            val productName = productInfo?.get("제품이름") as? String ?: "제품명 정보 없음"
+                            val productBrand = productInfo?.get("브랜드") as? String ?: "브랜드 정보 없음"
+                            val productPrice = productInfo?.get("가격") as? String ?: "가격 정보 없음"
+                            textView.text = "$productName\n$productBrand\n$productPrice"
+                        }
+                    }
+
+                    val eyeMap = productDescriptions.find { it.containsKey("아이") }
+                        ?.get("아이") as? Map<String, Any> ?: mapOf()
+                    eyeMap.let { products ->
+                        eyeProductTextViews.forEachIndexed { index, textView ->
+                            val productKey = "아이제품${index + 1}"
+                            val productInfo = products[productKey] as? Map<String, Any>
+                            val productName = productInfo?.get("제품이름") as? String ?: "제품명 정보 없음"
+                            val productBrand = productInfo?.get("브랜드") as? String ?: "브랜드 정보 없음"
+                            val productPrice = productInfo?.get("가격") as? String ?: "가격 정보 없음"
+                            textView.text = "$productName\n$productBrand\n$productPrice"
+                        }
+                    }
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(
+                    "FirestoreError",
+                    "Error fetching document with ID: $result",
+                    exception
+                ) // 오류 로그
+                Toast.makeText(
+                    context,
+                    "Error loading details: ${exception.localizedMessage}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
     }
 }
