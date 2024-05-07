088f8acfddecf476b0a6dcb7066c053de5f63ae3
Jiye Lee <80204749+dlwldP@users.noreply.github.com>
2024-05-07 17:43:03 +0900

    pull하기직전

--- app/src/main/java/com/example/mycolor/Fragment/MyPageFragment.kt
+++ app/src/main/java/com/example/mycolor/Fragment/MyPageFragment.kt
@@ -14,6 +14,7 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.example.mycolor.R
+import com.example.mycolor.login.LoginActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
@@ -42,10 +43,67 @@ class MyPageFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Toast.makeText(context, "뷰 생성 완료", Toast.LENGTH_SHORT).show()
 
         firebaseAuth = FirebaseAuth.getInstance()
         firestore = FirebaseFirestore.getInstance()
+
+        val nameTextView = view.findViewById<TextView>(R.id.customerName)
+        // ImageView 로컬 변수에 할당
+        val logoimageView = view.findViewById<ImageView>(R.id.logoimageView)
+        logoimageView.setImageResource(R.drawable.logoimage)
+
+        val auth = FirebaseAuth.getInstance()
+
+        // 현재 로그인한 사용자 정보 가져오기
+        val user = auth.currentUser
+        if (user != null) {
+            // 사용자 UID 출력
+            uid = user.uid
+            Log.d("FirebaseAuth", "User UID: $uid")
+            // 여기서 uid를 사용할 수 있습니다.
+        } else {
+            Log.d("FirebaseAuth", "No user is currently signed in.")
+        }
+
+        val db = FirebaseFirestore.getInstance()
+
+        // User 컬렉션에서 해당 uid의 문서를 참조합니다.
+        val userRef = db.collection("User").document(uid)
+
+        userRef.get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val document = task.result
+                if (document != null && document.exists()) {
+                    // 문서가 존재하고, name 필드에 값이 있을 경우 그 값을 가져옴
+                    val name = document.getString("name") ?: "상상부기" // name 필드가 null이면 "상상부기"를 사용
+                    nameTextView.text = name
+                    Log.d("Firestore", "Name: $name")
+                } else {
+                    // 문서가 존재하지 않을 경우, 기본값을 설정
+                    Log.d("Firestore", "No such document, setting default name to '상상부기'")
+                    // 필요한 경우 여기서 기본값을 문서에 설정할 수 있습니다.
+                }
+            } else {
+                Log.d("Firestore", "get failed with ", task.exception)
+            }
+        }
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+
+        // 로그아웃 버튼에 대한 참조 및 클릭 리스너 설정
+        val logoutButton = view.findViewById<Button>(R.id.button3)
+        logoutButton.setOnClickListener {
+            auth.signOut()
+            Toast.makeText(activity, "로그아웃", Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(context, LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+
+        }
+
+        fetchPersonalColorInfo()
 
         baseProductTextViews = listOf(
             view.findViewById(R.id.baseproductTextView_11),
@@ -63,30 +121,6 @@ class MyPageFragment : Fragment() {
             view.findViewById(R.id.eyeproductTextView_33)
         )
 
-        val auth = FirebaseAuth.getInstance()
-
-        val user = auth.currentUser
-        if (user != null) {
-            uid = user.uid
-            Log.d("FirebaseAuth", "User UID: $uid")
-        } else {
-            Log.d("FirebaseAuth", "No user is currently signed in.")
-        }
-
-        //기존코드
-        val logoimageView = view.findViewById<ImageView>(R.id.logoimageView)
-        logoimageView.setImageResource(R.drawable.logoimage)
-
-
-
-        val logoutButton = view.findViewById<Button>(R.id.button3)
-        logoutButton.setOnClickListener {
-            Toast.makeText(activity, "앱을 종료합니다.", Toast.LENGTH_SHORT).show()
-            logoutButton.postDelayed({
-                activity?.finish()
-            }, 1000)
-        }
-
         fetchUserUid(uid) { result ->
             if (result.startsWith("Error") || result == "No documents found") {
                 Toast.makeText(context, "Failed to load data: $result", Toast.LENGTH_LONG).show()
@@ -99,11 +133,12 @@ class MyPageFragment : Fragment() {
         fetchPersonalColorInfo()
     }
 
+
     private fun fetchPersonalColorInfo() {
         val uid = firebaseAuth.currentUser?.uid
-        Toast.makeText(context, "개인 색상 정보 확인 중...", Toast.LENGTH_SHORT).show()
+       // Toast.makeText(context, "개인 색상 정보 확인 중...", Toast.LENGTH_SHORT).show()
         if (uid == null) {
-            Toast.makeText(context, "로그인 상태가 아닙니다.", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(context, "로그인 상태가 아닙니다.", Toast.LENGTH_SHORT).show()
             return
         }
 
@@ -113,19 +148,18 @@ class MyPageFragment : Fragment() {
                 if (!documents.isEmpty) {
                     val document = documents.documents.first()
                     val colorResult = document.getString("result") ?: "Unknown"
-                    Toast.makeText(context, "색상 결과: $colorResult", Toast.LENGTH_LONG).show()
+                    //Toast.makeText(context, "색상 결과: $colorResult", Toast.LENGTH_LONG).show()
                     updateUI(colorResult.toLowerCase(Locale.ROOT))
                 } else {
-                    Toast.makeText(context, "결과 정보가 없습니다.", Toast.LENGTH_SHORT).show()
+                    //Toast.makeText(context, "결과 정보가 없습니다.", Toast.LENGTH_SHORT).show()
                 }
             }
             .addOnFailureListener {
-                Toast.makeText(context, "정보를 불러오는 데 실패했습니다: ${it.localizedMessage}", Toast.LENGTH_LONG).show()
+                //Toast.makeText(context, "정보를 불러오는 데 실패했습니다: ${it.localizedMessage}", Toast.LENGTH_LONG).show()
             }
     }
 
     private fun updateUI(colorResult: String) {
-        Toast.makeText(context, "UI 업데이트 중...", Toast.LENGTH_SHORT).show()
         val storageRef = FirebaseStorage.getInstance().reference
         val categories = listOf("base", "lip", "eye")
         categories.forEach { category ->
@@ -135,7 +169,7 @@ class MyPageFragment : Fragment() {
     }
 
     private fun getImageViewsByCategory(category: String): List<ImageView> {
-        Toast.makeText(context, "$category 카테고리의 이미지 뷰 로드 중...", Toast.LENGTH_SHORT).show()
+        //Toast.makeText(context, "$category 카테고리의 이미지 뷰 로드 중...", Toast.LENGTH_SHORT).show()
         return listOf(
             requireView().findViewById(resources.getIdentifier("${category}imageView_1", "id", context?.packageName)),
             requireView().findViewById(resources.getIdentifier("${category}imageView_2", "id", context?.packageName)),
@@ -149,10 +183,10 @@ class MyPageFragment : Fragment() {
             val imageRef = storageRef.child(imagePath)
             imageRef.downloadUrl.addOnSuccessListener { uri ->
                 Glide.with(this).load(uri).into(imageView)
-                Toast.makeText(context, "이미지 로드 성공: $imagePath", Toast.LENGTH_SHORT).show()
+                //Toast.makeText(context, "이미지 로드 성공: $imagePath", Toast.LENGTH_SHORT).show()
             }.addOnFailureListener { exception ->
                 Log.e("Storage", "Error loading image: $imagePath", exception)
-                Toast.makeText(context, "이미지 로드 실패: ${exception.localizedMessage}", Toast.LENGTH_LONG).show()
+                //Toast.makeText(context, "이미지 로드 실패: ${exception.localizedMessage}", Toast.LENGTH_LONG).show()
             }
         }
     }
@@ -173,13 +207,13 @@ class MyPageFragment : Fragment() {
                         result = document.getString("result") ?: "No result found"
                     }
                     result?.let { callback(it) }
-                    Toast.makeText(context, "사용자 UID 확인: $result", Toast.LENGTH_SHORT).show()
+                    //Toast.makeText(context, "사용자 UID 확인: $result", Toast.LENGTH_SHORT).show()
                 }
             }
             .addOnFailureListener { exception ->
                 Log.w("Firestore", "Error getting documents: ", exception)
                 callback("Error getting documents: ${exception.message}")
-                Toast.makeText(context, "문서 가져오기 실패: ${exception.localizedMessage}", Toast.LENGTH_LONG).show()
+                //Toast.makeText(context, "문서 가져오기 실패: ${exception.localizedMessage}", Toast.LENGTH_LONG).show()
             }
     }
 
